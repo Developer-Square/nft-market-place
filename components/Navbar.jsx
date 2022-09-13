ae57dd74a58576fddd54f9890dd01ad372401d6d
ryann254 <keddelyronjoz@gmail.com>
2022-09-13 18:03:09 +0300

    feat: add mobile menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -68,7 +68,9 @@ const ButtonGroup = ({ setActive, router }) => {
 const Navbar = () => {
 	const { theme, setTheme } = useTheme();
 	const [active, setActive] = useState("Explore NFTs");
+	const [isOpen, setIsOpen] = useState(false);
 	const router = useRouter();
+
 	return (
 		<nav className='flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1'>
 			<div className='flex flex-1 flex-row justify-start'>
@@ -120,8 +122,44 @@ const Navbar = () => {
 				</div>
 				<div className='md:hidden flex'>
 					<MenuItems active={active} setActive={setActive} />
-					<ButtonGroup setActive={setActive} router={router} />
+					<div className='ml-4'>
+						<ButtonGroup setActive={setActive} router={router} />
+					</div>
 				</div>
+			</div>
+			<div className='hidden md:flex mx-2'>
+				{isOpen ? (
+					<Image
+						src={images.cross}
+						objectFit='contain'
+						width={20}
+						height={20}
+						alt='close'
+						onClick={() => setIsOpen(false)}
+						className={theme === "light" && "filter invert"}
+					/>
+				) : (
+					<Image
+						src={images.menu}
+						objectFit='contain'
+						width={25}
+						height={25}
+						alt='menu'
+						onClick={() => setIsOpen(true)}
+						className={theme === "light" && "filter invert"}
+					/>
+				)}
+
+				{isOpen && (
+					<div className='fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col'>
+						<div className='flex-1 p-4'>
+							<MenuItems active={active} setActive={setActive} isMobile />
+						</div>
+						<div className='p-4 border-t dark:border-nft-black-1 border-nft-gray-1'>
+							<ButtonGroup setActive={setActive} router={router} />
+						</div>
+					</div>
+				)}
 			</div>
 		</nav>
 	);
