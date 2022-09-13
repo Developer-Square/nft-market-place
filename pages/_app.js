6ec9eb815a166481175c167a5ccc5c97b0f777a6
ryann254 <keddelyronjoz@gmail.com>
2022-09-13 17:02:09 +0300

    feat: added toggle button

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "next-themes";
+import Script from "next/script";
 import { Footer, Navbar } from "../components";
 
 import "../styles/globals.css";
@@ -6,10 +7,16 @@ import "../styles/globals.css";
 const MyApp = ({ Component, pageProps }) => {
 	return (
 		<ThemeProvider attribute='class'>
-			<div className='dark:bg-nft-dark bg-white min-h-screen'></div>
-			<Navbar />
-			<Component {...pageProps} />
-			<Footer />
+			<div className='dark:bg-nft-dark bg-white min-h-screen'>
+				<Navbar />
+				<Component {...pageProps} />
+				<Footer />
+			</div>
+
+			<Script
+				src='https://kit.fontawesome.com/d45b25ceeb.js'
+				crossorigin='anonymous'
+			></Script>
 		</ThemeProvider>
 	);
 };
